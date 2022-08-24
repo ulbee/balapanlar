8c2737607b4e92c8475038eead8595b3e34f1949
Yulia Berezhnaya <p.kotta@yandex.ru>
2022-08-24 23:39:48 +0300

    Добавлены брейкпоинты под планшеты и мобильные

--- src/index.js
+++ src/index.js
@@ -8,16 +8,24 @@ gsap.registerPlugin(ScrollTrigger);
 /**
  * Настройка анимации для галереи
  */
-let galleryItems = gsap.utils.toArray('.gallery__item');
+let mm = gsap.matchMedia();
 
-gsap.to(galleryItems, {
-    scrollTrigger: {
-        trigger: ".gallery", // триггерный блок для анимации
-        start: "top top", // начало анимации, когда триггер наверху экрана
-        scrub: 1, // "смягчение" перехода между анимацией и скроллом
-        snap: 0, // продолжение анимации после того, как пользователь перестал скроллить
-        pin: true // фиксация блока на определенном месте при скролле
-    },
-    xPercent: -100 * (galleryItems.length - 1), // анимация по горизонтали на всю ширину всех слайдов
-    ease: "none" // функция для определения скорости отображения анимации
+mm.add("(min-width: 801px)", () => {
+    let galleryItems = gsap.utils.toArray('.gallery__item');
+
+    console.log(-100 * (galleryItems.length - 1));
+
+    gsap.to(galleryItems, {
+        scrollTrigger: {
+            trigger: ".gallery", // триггерный блок для анимации
+            start: "top top", // начало анимации, когда триггер наверху экрана
+            scrub: 1, // "смягчение" перехода между анимацией и скроллом
+            snap: 0, // продолжение анимации после того, как пользователь перестал скроллить
+            pin: true // фиксация блока на определенном месте при скролле
+        },
+        xPercent: -100 * (galleryItems.length - 1), // анимация по горизонтали на всю ширину всех слайдов
+        ease: "none" // функция для определения скорости отображения анимации
+    });
+    
 });
+
