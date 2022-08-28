833a3a22c259e9f94d7dedd263d91e2efc1ec518
Stacy985» git config --global user.email «Anastasia.disy@gmail.com»git config --global user.name Stacy985» <stasbasov@yandex.ru>
2022-08-28 18:47:13 +0300

    js хэдера

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-import './pages/index.css';
+import "./pages/index.css";
+const header = document.querySelector(".header");
+const logo = header.querySelector(".header__logo");
+const headerHeight = header.clientHeight;
+
+document.onscroll = function () {
+  let scroll = window.scrollY;
+  if (scroll > headerHeight) {
+    header.classList.add("header_scroll");
+    logo.classList.add("header__logo_scroll");
+    document.body.style.paddingTop = headerHeight + "px";
+  } else {
+    header.classList.remove("header_scroll");
+    logo.classList.remove("header__logo_scroll");
+    document.body.removeAttribute("style");
+  }
+};
