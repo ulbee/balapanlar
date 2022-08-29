751853c0b727261a679dd3e94b19f3bd826c05ca
Stacy985» git config --global user.email «Anastasia.disy@gmail.com»git config --global user.name Stacy985» <stasbasov@yandex.ru>
2022-08-29 23:25:57 +0300

    Сделала анимацию бургера

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const headerHeight = header.clientHeight;
 document.onscroll = function () {
   let scroll = window.scrollY;
 
-  console.log('headerHeight', headerHeight);
+  console.log("headerHeight", headerHeight);
   if (scroll > headerHeight) {
     header.classList.add("header_scroll");
     logo.classList.add("header__logo_scroll");
@@ -24,24 +24,29 @@ document.onscroll = function () {
   }
 };
 
+/* Бургер меню */
+const burgerButton = document.querySelector(".header__burger");
+
+burgerButton.addEventListener("click", () => {
+  header.classList.toggle("header_burger-menu-open");
+});
+
 /**
  * Настройка анимации для галереи
  */
 let mm = gsap.matchMedia();
-let galleryItems = gsap.utils.toArray('.gallery__item');
+let galleryItems = gsap.utils.toArray(".gallery__item");
 
-mm.add("(min-width: 801px)", () => {    
-
-    gsap.to(galleryItems, {
-        scrollTrigger: {
-            trigger: ".gallery", // триггерный блок для анимации
-            start: "top top", // начало анимации, когда триггер наверху экрана
-            scrub: 1, // "смягчение" перехода между анимацией и скроллом
-            snap: 0, // продолжение анимации после того, как пользователь перестал скроллить
-            pin: true // фиксация блока на определенном месте при скролле
-        },
-        xPercent: -100 * (galleryItems.length - 1), // анимация по горизонтали на всю ширину всех слайдов
-        ease: "none" // функция для определения скорости отображения анимации
-    });
-    
+mm.add("(min-width: 801px)", () => {
+  gsap.to(galleryItems, {
+    scrollTrigger: {
+      trigger: ".gallery", // триггерный блок для анимации
+      start: "top top", // начало анимации, когда триггер наверху экрана
+      scrub: 1, // "смягчение" перехода между анимацией и скроллом
+      snap: 0, // продолжение анимации после того, как пользователь перестал скроллить
+      pin: true, // фиксация блока на определенном месте при скролле
+    },
+    xPercent: -100 * (galleryItems.length - 1), // анимация по горизонтали на всю ширину всех слайдов
+    ease: "none", // функция для определения скорости отображения анимации
+  });
 });
