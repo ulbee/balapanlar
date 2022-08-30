a0362de857c730b16b00581b8b0618a694b9caaf
alekseydegtyarev <alekseydegtyarev41rus@gmail.com>
2022-08-30 19:16:42 +1200

    открытие/закрытие попапов, мелкие правки

--- src/index.js
+++ src/index.js
@@ -45,3 +45,52 @@ mm.add("(min-width: 801px)", () => {
     });
     
 });
+
+
+const viewSkillBtn = document.querySelector('h1'), //нужен класс кнопки "Смотреть все навыки"
+  popupCourses = document.querySelector('#course-python'),
+  partnerLWBBtn = document.querySelector('#lecturers'),
+  partnerOpenHorizonsBtn = document.querySelector('#horizont'), //
+  partnerChemBioBtn = document.querySelector('#himbio'), //
+  popupLWB = document.querySelector('#partner-lecturers'), //
+  popupOpenHorizon = document.querySelector('#partner-horizont'), //
+  popupChemBio = document.querySelector('#partner-himbio'),
+  popupCloseBtn = document.querySelector('.popup__close-btn'),
+  closeBtn = document.querySelector('.popup__close');
+
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+  window.addEventListener('keydown', handleEsc);
+  window.addEventListener('mousedown', closeByOverlay);
+  closeBtn.addEventListener('click', () => closePopup(popupCourses));
+  popupCloseBtn.addEventListener('click', () => closePopup(popupCourses));
+}
+
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+  window.removeEventListener('keydown', handleEsc);
+  window.removeEventListener('click', closeByOverlay);
+  closeBtn.removeEventListener('click', () => closePopup(popupCourses));
+  popupCloseBtn.removeEventListener('click', () => closePopup(popupCourses));
+}
+
+//закрытие по esc
+function handleEsc(evt) {
+  if ((evt.key === 'Escape') && (document.querySelector('.popup_opened'))) { //проверяем, что (нажат Esc) и (на странице присутствует открытый попап)
+    const popup = document.querySelector('.popup_opened');
+    closePopup(popup);
+  }
+}
+
+//Закрытие кликом на оверлей
+function closeByOverlay (evt) {
+  if (evt.target.classList.contains('.popup')) {
+    closePopup(evt.target);
+  }
+}
+
+
+viewSkillBtn.addEventListener('click', () => openPopup(popupCourses));
+partnerLWBBtn.addEventListener('click', () => openPopup(popupLWB));
+partnerOpenHorizonsBtn.addEventListener('click', () => openPopup(popupOpenHorizon));
+partnerChemBioBtn.addEventListener('click', () => openPopup(popupChemBio));
